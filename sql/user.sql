c9cb5853c52cf0753b76159a4137ebefcb9e87c4
tanya55 <58249222+tanya55@users.noreply.github.com>
2021-02-15 00:41:31 +0200

    create user

---
+++ sql/user.sql
@@ -0,0 +1,13 @@
+create table hr_system.tb_users(
+	id integer auto_increment primary key,
+	user_name varchar(256),
+	first_name varchar(256),
+	last_name varchar(256),
+	email varchar(256),
+	password varchar(256),
+	number varchar(10),
+	age varchar(256),
+	gender varchar(256),
+	city varchar(256),
+	category varchar(256)
+);
